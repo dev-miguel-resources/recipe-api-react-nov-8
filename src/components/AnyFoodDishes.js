41b412d772d442075d579c19579900cfce15ee4f
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-06 21:42:16 -0300

    AnyFoodDishes, FoodDishes complete and working on FoodDish

--- src/components/AnyFoodDishes.js
+++ src/components/AnyFoodDishes.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import TextField  from "@material-ui/core/TextField";
+import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
@@ -10,11 +10,37 @@ import { anyFoodDishes } from "../constants";
 const AnyFoodDishes = ({ getData }) => {
   const [query, setQuery] = useState();
 
-  const getFoods = () => {};
+  const getFoods = () => {
+    const title = document
+      .querySelector("#AnyFoodDishes")
+      .value.toLowerCase()
+      .trim();
+    if (query !== title) {
+      setQuery(title);
+      getData(title);
+    }
+  };
 
   return (
-    <Grid>
-      <Paper></Paper>
+    <Grid item xs={12} sm={6}>
+      <Paper className="paper">
+        <Autocomplete
+          id="AnyFoodDishes"
+          freeSolo
+          options={anyFoodDishes.map((foodDish) => foodDish.title)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Food"
+              margin="normal"
+              variant="outlined"
+            />
+          )}
+        />
+        <IconButton onClick={() => getFoods()}>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </Grid>
   );
 };
